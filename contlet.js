ca89a077a2646e994ebd0623dd489d0cf6200fda
esther <5e2sther0@gmail.com>
2024-05-28 15:04:40 +0900

    arr와 object에 대한 개념

---
+++ contlet.js
@@ -0,0 +1,59 @@
+let _month = [2, 3, 7]
+let _study= ['피그마','리액트','뷰']
+let _name =['홍길동','나훈아','조명섭']
+
+const myword0 = `저는 ${_month[0]}개월간 ${_study[0]} 공부를 한 ${_name[0]} 입니다!`;
+const myword1 = `저는 ${_month[1]}개월간 ${_study[1]} 공부를 한 ${_name[1]} 입니다!`;
+const myword2 = `저는 ${_month[2]}개월간 ${_study[2]} 공부를 한 ${_name[2]} 입니다!`;
+console.log(myword0 + myword1 + myword2)
+
+//저는 2개월간의 피그마 공부를 한 홍길동입니다!
+//저는 3개월간의 뷰 공부를 한 나훈아입니다!
+//저는 7개월간의 리액트 공부를 한 조명섭입니다!
+
+const myarr = [
+    ["네이버","http://www.naver.com"],
+    ["다음","http://www.daum.net"],
+    ["구글","http://www.google.com"]
+]
+console.log("첫번째 배열",myarr[0], typeof myarr[0],
+"첫번째배열의 첫번째값",myarr[0][0], typeof myarr[0][0] );
+
+console.log(`<a href='${myarr[0][1]}'>${myarr[0][0]}</>`)
+console.log(`<a href='${myarr[1][1]}'>${myarr[1][0]}</>`)
+console.log(`<a href='${myarr[2][1]}'>${myarr[2][0]}</>`)
+
+
+const esther = [
+    ["하나","동대문", "홍길동"],
+    ["신한","천호", "방보영"],
+    ["우리","안국","돌돌이"]
+]
+
+console.log(esther[1][2])
+
+const myarrobj = {
+    _month : [2,3,7],
+    _study : ['피그마','리액트','뷰'],
+    _name :  ['홍길동','나훈아','조명섭']
+}
+
+console.log(myarrobj._name[2])
+
+const gnb={
+    d1:{
+        text : "회사소개",
+        href : "./company.html"
+    }, 
+    d2 :[
+        {
+            text : "CEO",
+            href : "./company.html"
+        },
+        {
+            text : "커뮤니티",
+            href : "./company.html"
+        }
+    ]  
+}
+console.log(gnb.d2[1].text)
